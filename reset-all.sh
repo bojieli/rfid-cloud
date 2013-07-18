d848a57b36431710900edb834e82f7c3d24a4d77
Bojie Li <boj@shi6.com>
2013-07-18 21:49:03 +0800

    fix bash: determine whether command exists

--- reset-all.sh
+++ reset-all.sh
@@ -5,11 +5,18 @@ echo -n "Press ENTER to continue or Ctrl+C to exit..."
 read
 
 # reset database
-mysql -u root -p -e "DROP DATABASE ecard;"
-mysql -u root -p < install.sql
+echo "DROP DATABASE ecard;" >reset.sql
+cat install.sql >>reset.sql
+echo "GRANT ALL ON ecard.* to 'ecard-www'@'localhost' identified by '0CIigA0nStYlEGMM'" >>reset.sql
+mysql -u root -p <reset.sql
+rm -f reset.sql
 
 # restart server
 pkill ecard
-command -v node >/dev/null 2>&1 && node ecard.js && exit 0
-command -v nodejs >/dev/null 2>&1 && nodejs ecard.js && exit 0
-echo "nodejs is required!" && exit 1
+if [ ! -z `command -v node` ]; then
+    node ecard.js
+elif [ ! -z `command -v nodejs` ]; then
+    nodejs ecard.js
+else
+    echo "nodejs is required!"
+fi
