617cbcf6c55f5f7d37adda372733ed00a10cb7aa
Bojie Li <boj@shi6.com>
2013-09-01 21:10:04 +0800

    remove empty output lines in monitor.js

--- monitor.js
+++ monitor.js
@@ -1,10 +1,10 @@
 function start() {
     var ls = require('child_process').spawn('node', ['ecard.js']);
     ls.stdout.on('data', function(data) {
-        console.log(data.toString());
+        process.stdout.write(data.toString());
     });
     ls.stderr.on('data', function(data) {
-        console.log(data.toString());
+        process.stdout.write(data.toString());
     });
     ls.on('exit', function(code) {
         console.log('child process exited with code ' + code);
