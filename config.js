5ea1d25beff7b0dc4b4d34a7ccca27f85cbc2aa0
Bojie Li <boj@shi6.com>
2013-10-14 20:07:12 +0800

    config heartbeat timeout to 300s

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var toexport = {
     listen_host: 'localhost',
     listen_port: 54322,
-    heartbeat_timeout: 30,  // in seconds
+    heartbeat_timeout: 300,  // in seconds
     max_reports_per_day: 10,
     admin_mobiles: ['18715009901', '15556980100'],
     sms_suffix: ' [电子学生证]',
